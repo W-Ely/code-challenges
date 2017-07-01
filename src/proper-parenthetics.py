54e88bb5cde8c051cd5d81321afe32f25d935245
W. Ely Paysinger <paysinger@gmail.com>
2017-06-30 19:53:10 -0700

    begining work.

---
+++ src/proper-parenthetics.py
@@ -0,0 +1,55 @@
+"""Module is a challenge to determine proper-paranthetics."""
+
+
+class Node(object):
+    """Node data structure."""
+
+    def __init__(self, value=None, next_node=None):
+        """Init a node."""
+        self.value = value
+        self.next_node = next_node
+
+
+class Stack(object):
+    """Create a stack data structure."""
+
+    def __init__(self, iterable=None):
+        """Init a stack."""
+        self.head = None
+
+    def push(self, val):
+        """Add item to stacl."""
+        node = Node(val, self.head)
+        self.head = node
+
+    def pop(self):
+        """Remove item from head and return value."""
+        if self.head:
+            val = self.head.value
+            self.head = self.head.next_node
+            return val
+
+
+def proper_paranthetics(string):
+    """Check a string for proper parenthic usage.
+
+    Return 1 if the string is 'open'.
+    Return 0 if the string is 'balanced'.
+    Return -1 if the string is 'broken'.
+    """
+    stack = Stack()
+    ref_in = {
+        '(': ')', '[': ']', '<': '>', '"': '"', '{': '}', '`': '`'
+    }
+    ref_out = {
+        ')', ']', '>', '"', "}", '`'
+    }
+    last_in = None
+    for chr in string:
+        if chr in ref_in:
+            stack.push(chr)
+        if chr in ref_out:
+            first_out = stack.pop()
+    return -1
+    # return 0
+    # return 1
